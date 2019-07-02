fe82e7955c7faa00f1e11a553493850085851f19
Sedzay <es281082@ukr.net>
2019-07-02 21:34:05 +0300

    init commit

--- src/main/java/lesson2/Solution.java
+++ src/main/java/lesson2/Solution.java
@@ -8,21 +8,50 @@ public class Solution {
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.ccepy0l4ctc8.us-east-2.rds.amazonaws.com:1521:ORCL";
 
     private static final String USER = "main";
-    private static final String PASS = "EkimovSergey";
+    private static final String PASS = "EkimovSergej";
 
-    public ArrayList<Product> getAllProducts() {
-        String query = "SELECT * FROM PRODUCT";
-        return executeQuery(query);
+    //void increasePrice() - увеличивает значение цены на 100, во всех продуктах, где цена меньше 970
+
+    public void increasePrice() {
+        String query = "UPDATE PRODUCT SET price = price + 100 WHERE price < 970";
+        executeQueryUpdate(query);
     }
 
-    public ArrayList<Product> getProductsByPrice() {
-        String query = "SELECT * FROM PRODUCT WHERE PRICE <=100";
-        return executeQuery(query);
+
+    //void changeDescription() - удаляет последнее предложение с описания всех продуктов с длинной описания больше 100
+
+    public void changeDescription() {
+        //1. Отобрать все продукты, у которых в описании больше 100 символов  +
+        //2. Разделить такое описание по предложениям  +
+        //3. Удалить последнее предложение  +
+        //4. Обновить описание в продукте в базе данных  +
+
+        ArrayList<Product> productArrayList = getProductsByDescription();
+
+        for (Product product: productArrayList) {
+
+            String[]allStrings = product.getDescription().split("\\.");
+
+            if(allStrings.length > 1) {
+                allStrings[allStrings.length-1] = null;
+                String newDescription = null;
+                for (String s : allStrings) {
+                    if(newDescription == null)
+                        newDescription = s;
+                    else if (s != null){
+                        newDescription += ".";
+                        newDescription += s;
+                    }
+                }
+                String query = "UPDATE PRODUCT SET description = " + "\'"+ newDescription + "\'"+ " WHERE " + product.getId() + " = id";
+                executeQueryUpdate(query);
+            }
+        }
     }
 
-    public ArrayList<Product> getProductsByDescription() {
+    private ArrayList<Product> getProductsByDescription() {
 
-        String query = "SELECT x.ID, x.NAME, x.DESCRIPTION, x.PRICE FROM PRODUCT x, (SELECT ID, LENGTH(DESCRIPTION) as len FROM PRODUCT) y WHERE x.ID = y.ID AND y.len > 50";
+        String query = "SELECT ID, NAME, DESCRIPTION, PRICE FROM PRODUCT WHERE LENGTH(DESCRIPTION) > 100";
         return executeQuery(query);
     }
 
@@ -44,6 +73,18 @@ public class Solution {
         return products;
     }
 
+    private void executeQueryUpdate(String query) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()){
+
+            int response = statement.executeUpdate(query);
+            System.out.println(response);
+
+        }catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
+    }
+
     private Product getProduct(ResultSet resultSet) throws SQLException{
         long id = resultSet.getLong(1);
         String name = resultSet.getString(2);
@@ -51,5 +92,20 @@ public class Solution {
         int price = resultSet.getInt(4);
 
         return new Product(id, name, description, price);
+    }
+
+
+
+
+//------------------------------------------------------
+    //из предыдущих заданий
+    public ArrayList<Product> getAllProducts() {
+        String query = "SELECT * FROM PRODUCT_";
+        return executeQuery(query);
+    }
+
+    public ArrayList<Product> getProductsByPrice() {
+        String query = "SELECT * FROM PRODUCT_ WHERE PRICE <=100";
+        return executeQuery(query);
     }
 }
--- src/test/java/Test_hw2.java
+++ src/test/java/Test_hw2.java
@@ -1,8 +1,11 @@
 import lesson2.Solution;
 
-public class Test_hw1 {
+
+public class Test_hw2 {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.getProductsByPrice());
+        //solution.increasePrice();
+        solution.changeDescription();
     }
+
 }
